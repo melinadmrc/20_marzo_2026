123b994552dfc7252093ae183883636873fb66cb
Melina <melina.dmec@gmail.com>
2026-03-22 11:20:25 +0100

    notas de estudio

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,29 +35,36 @@ console.log(nombres);
 // indexOf() → busca un valor en un array
 const posicion = nombres.indexOf("Melina");
 
-console.log("Melina");
+console.log(posicion);
 
 //---------------
 // EJERCICIO 2 - ARREGLO DE COLORES
-const colores = [];
-
-// agregar valores posición - color
-colores[0] = "red";
-colores[1] = "blue";
-colores[2] = "green";
-colores[3] = "yellow";
-colores[4] = "purple";
-colores[5] = "black";
-colores[6] = "brown";
+const colores = ["red", "blue", "green", "yellow", "purple", "black", "brown"];
 
 console.log(colores);
 
 function cambiarColor() {
-
-  const posicion = 4;
+  const posicion = 6;
 
   console.log(colores[posicion]);
 
   // cambia color del background
   document.body.style.backgroundColor = colores[posicion];
 }
+
+//---------------
+// EJERCICIO 2 - ARREGLO DE COLORES - con select
+
+const select = document.getElementById("select");
+
+select.addEventListener("change", () => {
+  document.body.style.backgroundColor = colores[select.value];
+});
+// select.value → obtiene el valor seleccionado ("0", "1", etc.)
+
+// colores[select.value]
+// Usa ese valor como posición del arreglo
+
+// Ejemplo:
+// Si elige "3"
+// colores[3] = "yellow"
